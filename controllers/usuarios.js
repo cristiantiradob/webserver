7f140d1fc27645b47fb700349bdb983a73391385
cotirado <cristiantiradoo@hotmail.com>
2021-08-14 21:48:04 -0500

    Se crea middleware para validaciones

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,31 +1,26 @@
 const { response } = require('express');
 const bcryptjs = require('bcryptjs');
-const { validationResult } = require('express-validator');
 const Usuario = require('../models/usuario');
 
 const controller = {
     usuariosGet: (req, res) => {
         // Obteniendo querys
-        const {q, nombre, apikey} = req.query;
+        const { q, nombre, apikey } = req.query;
 
-        return res.status(200).send({"q": q, "nombre": nombre, "apikey": apikey});
+        return res.status(200).send({ "q": q, "nombre": nombre, "apikey": apikey });
     },
     usuariosPut: (req, res) => {
-        return res.status(200).send({msg: 'get API - controlador'});
+        return res.status(200).send({ msg: 'get API - controlador' });
     },
-    usuariosPost: async(req, res) => {
-        const errors = validationResult(req);
-        if(!errors.isEmpty()){
-            return res.status(400).json(errors);
-        }
+    usuariosPost: async (req, res) => {
 
-        const {nombre, correo, password, rol} = req.body;
-        const usuario = new Usuario({nombre,correo,password,rol});
+        const { nombre, correo, password, rol } = req.body;
+        const usuario = new Usuario({ nombre, correo, password, rol });
 
         // Verificar si el correo existe
-        const existeEmail = await Usuario.findOne({correo});
-        if(existeEmail){
-            return res.status(400).send({msg: 'El correo ya está registrado'});
+        const existeEmail = await Usuario.findOne({ correo });
+        if (existeEmail) {
+            return res.status(400).send({ msg: 'El correo ya está registrado' });
         }
 
         // Encriptar la contraseña
@@ -35,10 +30,10 @@ const controller = {
         // Guardar en DB
         await usuario.save();
 
-        return res.status(200).send({usuario});
+        return res.status(200).send({ usuario });
     },
     usuariosDelete: (req, res) => {
-        return res.status(200).send({msg: 'get API - controlador'});
+        return res.status(200).send({ msg: 'get API - controlador' });
     }
 }
 
--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,18 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 const router = Router();
-const userControllers = require('../controllers/usuarios')
+const userControllers = require('../controllers/usuarios');
+const validarCampos = require('../middlewares/validar-campos');
 
 router.get('/', userControllers.usuariosGet)
 router.put('/', userControllers.usuariosPut)
 router.post('/', [
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    check('password', 'El password debe ser más de 6 letras').isLength({ min: 6 }),
     check('correo', 'El correo no es válido').isEmail(),
-] ,userControllers.usuariosPost)
+    check('rol', 'No es un rol válido').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+    validarCampos
+], userControllers.usuariosPost)
 router.delete('/', userControllers.usuariosDelete)
 
 module.exports = router;
